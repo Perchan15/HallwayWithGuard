8b79e10d3034002c4d85b633eda1f3841b590662
Ronald Chancey <perchan@Knights.ucf.edu>
2022-03-06 16:50:01 -0500

    tweakedExitScript

--- Assets/Scripts/MenuScripts/EscapeToExit.cs
+++ Assets/Scripts/MenuScripts/EscapeToExit.cs
@@ -8,7 +8,7 @@ public class EscapeToExit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("escape"))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
         Application.Quit();
         }
